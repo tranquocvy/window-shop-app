f7ee09ac9def07953ed8af34f1621247fad34062
nphoang <nphuchoang.itus@gmail.com>
2025-11-01 19:48:10 +0700

    feat(domain): add product entity

--- TechHaven/TechHaven.Domain/Entities/Product.cs
+++ TechHaven/TechHaven.Domain/Entities/Product.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TechHaven.Domain.Entities;
 
-namespace TechHaven.Domain.Entities
+public class Product
 {
-    internal class Product
-    {
-    }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public string BrandName { get; set; } = string.Empty;
+    public decimal CostPrice { get; set; }
+    public decimal SellPrice { get; set; }
+    public int StockQuantity { get; set; } = 0;
+    public string? Description { get; set; }
+    public bool IsDraft { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+
+    // Navigation
+    public Category? Category { get; set; }
 }
