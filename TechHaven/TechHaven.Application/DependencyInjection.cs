199de784ae5a281a94801c5c214631c9a5da81d9
nphoang <nphuchoang.itus@gmail.com>
2025-11-09 00:25:47 +0700

    feat(feature/app-cqrs): update DI

--- TechHaven/TechHaven.Application/DependencyInjection.cs
+++ TechHaven/TechHaven.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using TechHaven.Application.Common.Behaviors;
 
 namespace TechHaven.Application;
 
@@ -7,10 +10,23 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // Đăng ký AutoMapper với tất cả Profiles trong Assembly hiện tại
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        var assembly = Assembly.GetExecutingAssembly();
         
-        // Đăng ký các services khác ở đây
+        // Đăng ký AutoMapper với tất cả Profiles trong Assembly hiện tại
+        services.AddAutoMapper(assembly);
+        
+        // Đăng ký MediatR với tất cả handlers trong Assembly
+        services.AddMediatR(cfg => 
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+        });
+        
+        // Đăng ký FluentValidation validators
+        services.AddValidatorsFromAssembly(assembly);
+        
+        // Đăng ký Pipeline Behaviors (thứ tự quan trọng - chạy theo thứ tự đăng ký)
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         
         return services;
     }
