6a6895509889fd7fe8d5225c01f2661d28cde025
nphoang <nphuchoang.itus@gmail.com>
2025-11-09 00:07:01 +0700

    feat(feature/app-cqrs): created CQRS interface

---
+++ TechHaven/TechHaven.Application/Common/Interfaces/IQueryHandler.cs
@@ -0,0 +1,12 @@
+using MediatR;
+
+namespace TechHaven.Application.Common.Abstractions;
+
+/// <summary>
+/// Handler for queries that return a result.
+/// Query handlers should never modify state.
+/// </summary>
+/// <typeparam name="TQuery">The type of query to handle.</typeparam>
+/// <typeparam name="TResponse">The type of result returned.</typeparam>
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse> { }
