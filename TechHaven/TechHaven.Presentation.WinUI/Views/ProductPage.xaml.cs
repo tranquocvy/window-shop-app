278a59b4dc9b29fe8fc09c22b4e35206bbadeb67
Phuc Hau <phuchau.fit@gmail.com>
2025-11-19 08:20:58 +0700

    feat(product): add product page layout without data binding

--- TechHaven/TechHaven.Presentation.WinUI/Views/ProductPage.xaml.cs
+++ TechHaven/TechHaven.Presentation.WinUI/Views/ProductPage.xaml.cs
@@ -1,31 +1,68 @@
+﻿using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace TechHaven.Presentation.WinUI.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    // LƯU Ý QUAN TRỌNG: Tên lớp phải là 'partial' và khớp chính xác với x:Class trong XAML
     public sealed partial class ProductPage : Page
     {
+        // Khai báo một danh sách sản phẩm mẫu để gán vào ListView nếu cần
+        // public ObservableCollection<Product> Products { get; set; } 
+
         public ProductPage()
         {
-            InitializeComponent();
+            // Hàm tạo này là cần thiết và phải được định nghĩa DUY NHẤT 1 lần.
+            this.InitializeComponent();
+
+            // Nếu bạn sử dụng MVVM, bạn sẽ gán ViewModel ở đây
+            // this.DataContext = new ProductViewModel(); 
+
+            // Ví dụ về việc tải dữ liệu tĩnh nếu không dùng Data Binding
+            // LoadStaticData(); 
         }
+
+        // -------------------------------------------------------------------
+        // PHƯƠNG THỨC XỬ LÝ SỰ KIỆN TỪ XAML
+        // -------------------------------------------------------------------
+
+        /// <summary>
+        /// Xử lý sự kiện khi nhấn nút "Thêm Sản Phẩm"
+        /// </summary>
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Viết logic để mở cửa sổ/dialog thêm sản phẩm mới
+
+            // Ví dụ: Hiện thông báo đơn giản
+            // (Bạn cần có một TextBlock hoặc thông báo trong UI để hiển thị)
+            // System.Diagnostics.Debug.WriteLine("Nút Thêm Sản Phẩm đã được nhấn!");
+        }
+
+        /// <summary>
+        /// Xử lý sự kiện khi nhấp vào một mục (Item) trong ProductsList
+        /// </summary>
+        private void ProductsList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // TODO: Viết logic để điều hướng đến trang chi tiết sản phẩm hoặc mở dialog chỉnh sửa
+
+            // Lấy dữ liệu của Item được click
+            // var selectedProduct = e.ClickedItem as Product; 
+
+            // if (selectedProduct != null)
+            // {
+            //     System.Diagnostics.Debug.WriteLine($"Sản phẩm được chọn: {selectedProduct.Name}");
+            //     // Ví dụ: Navigation.Frame.Navigate(typeof(ProductDetailPage), selectedProduct);
+            // }
+        }
+
+        // -------------------------------------------------------------------
+        // CÁC PHƯƠNG THỨC HỖ TRỢ KHÁC (Tùy chọn)
+        // -------------------------------------------------------------------
+
+        // private void LoadStaticData()
+        // {
+        //     // Logic để tạo dữ liệu tĩnh và gán vào ListView (nếu không dùng ItemTemplate tĩnh)
+        // }
     }
 }
