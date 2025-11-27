b24fbf3322b61aaf59db866831130f30e5715910
23120410 <quocvy23072005@gmail.com>
2025-11-27 19:33:59 +0700

    feat(dashboard-view): completed dashboard view

--- TechHaven/TechHaven.Presentation.WinUI/Views/DashboardPage.xaml.cs
+++ TechHaven/TechHaven.Presentation.WinUI/Views/DashboardPage.xaml.cs
@@ -2,15 +2,13 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using TechHaven.Presentation.WinUI.ViewModel;
+using Microsoft.UI.Dispatching;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace TechHaven.Presentation.WinUI.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DashboardPage : Page
     {
         private readonly DashboardViewModel _vm = new();
@@ -19,12 +17,23 @@ namespace TechHaven.Presentation.WinUI.Views
         {
             InitializeComponent();
             this.DataContext = _vm;
+            this.Loaded += DashboardPage_Loaded;
+            MonthlyChartBorder.SizeChanged += MonthlyChartBorder_SizeChanged;
         }
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        private async void DashboardPage_Loaded(object sender, RoutedEventArgs e)
         {
-            base.OnNavigatedTo(e);
+            this.Loaded -= DashboardPage_Loaded;
             await _vm.LoadAsync();
+
+            // Enqueue geometry update after layout pass to ensure ActualWidth/Height are valid
+            var dq = DispatcherQueue.GetForCurrentThread();
+            dq?.TryEnqueue(() => _vm.UpdateMonthlyRevenueGeometry(MonthlyChartBorder.ActualWidth, MonthlyChartBorder.ActualHeight));
+        }
+
+        private void MonthlyChartBorder_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _vm.UpdateMonthlyRevenueGeometry(MonthlyChartBorder.ActualWidth, MonthlyChartBorder.ActualHeight);
         }
     }
 }
