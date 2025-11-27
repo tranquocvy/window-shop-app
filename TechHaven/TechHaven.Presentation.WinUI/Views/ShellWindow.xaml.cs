093cd87728de948e7e1d0c42959cc4f5b8185087
23120410 <quocvy23072005@gmail.com>
2025-11-28 00:56:27 +0700

    feat(theme): updated shellwindow view with theme

--- TechHaven/TechHaven.Presentation.WinUI/Views/ShellWindow.xaml.cs
+++ TechHaven/TechHaven.Presentation.WinUI/Views/ShellWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using TechHaven.Presentation.WinUI.Helpers;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using TechHaven.Presentation.WinUI.Themes;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,6 +18,12 @@ namespace TechHaven.Presentation.WinUI.Views
         public ShellWindow()
         {
             this.InitializeComponent();
+
+            if (Content is FrameworkElement root)
+            {
+                ThemeManager.ApplyTo(root);
+            }
+
             if (AppState.CurrentUser != null)
             {
                 // Lấy dữ liệu từ AppState
