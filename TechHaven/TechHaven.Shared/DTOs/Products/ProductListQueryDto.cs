25307b1d3c1ad1dd11a52d18c0931a38bdb0e80b
Phuc Hau <phuchau.fit@gmail.com>
2025-11-28 19:31:12 +0700

    feat: add filter attribute in Product DTO

--- TechHaven/TechHaven.Shared/DTOs/Products/ProductListQueryDto.cs
+++ TechHaven/TechHaven.Shared/DTOs/Products/ProductListQueryDto.cs
@@ -1,4 +1,4 @@
-using TechHaven.Shared.DTOs.Common;
+﻿using TechHaven.Shared.DTOs.Common;
 
 namespace TechHaven.Shared.DTOs.Products;
 
@@ -7,6 +7,16 @@ public class ProductListQueryDto : PagingRequest
     public string? SearchTerm { get; set; }
 
     public bool? IsDraft { get; set; }
+
+    //Lọc theo khoảng giá
+    public int? fromPrice { get; set; }
+    public int? toPrice { get; set; }
+
+    //Lọc theo hãng
+    public string? Brand { get; set; }
+
+    //Lọc theo trạng thái
+    public string? Status { get; set; }
 
     public SortingOption? Sorting { get; set; }
 }
