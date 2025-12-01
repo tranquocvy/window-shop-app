fa76b56e3470ea03806b0062fa2eb306cb757292
= <ngduong5124@gmail.com>
2025-12-02 00:10:58 +0700

    feat(loggin): synchronized from branch logging

--- TechHaven/TechHaven.Infrastructure/Persistence/Repositories/OrderRepository.cs
+++ TechHaven/TechHaven.Infrastructure/Persistence/Repositories/OrderRepository.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Extensions.Logging;
 using TechHaven.Domain.Entities;
 using TechHaven.Domain.Interfaces;
 using TechHaven.Domain.SearchCriteria;
 using TechHaven.Domain.Specifications;
 using TechHaven.Infrastructure.Persistence;
+using Org.BouncyCastle.Math.EC.Rfc7748;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace TechHaven.Infrastructure.Persistence.Repositories;
 
@@ -12,7 +16,7 @@ namespace TechHaven.Infrastructure.Persistence.Repositories;
 /// </summary>
 public class OrderRepository : GenericRepository<Order>, IOrderRepository
 {
-    public OrderRepository(AppDbContext context) : base(context)
+    public OrderRepository(AppDbContext context, ILoggerFactory loggerFactory) : base(context, loggerFactory)
     {
     }
     // 1. Hàm Search cho danh sách 
@@ -21,6 +25,9 @@ public class OrderRepository : GenericRepository<Order>, IOrderRepository
         CancellationToken cancellationToken)
     {
         var spec = new OrderSearchSpecification(criteria);
+        _logger.LogInformation(
+           "Searching orders with criteria {@Criteria}",
+           criteria);
 
         var items = await GetAsync(spec, cancellationToken);
 
@@ -33,6 +40,7 @@ public class OrderRepository : GenericRepository<Order>, IOrderRepository
 
     public async Task<Order?> GetWithDetailsAsync(int orderId, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation("Fetching order detail with order id {orderId}", orderId);
         return await _dbSet
             .Include(o => o.Customer)
             .Include(o => o.User)
